6dd1eb5e929ce6e16635d06b7197811a8bfe1824
alfathsurya <subchan.arif81@gmail.com>
2023-01-20 23:10:26 +0700

    Update miner.py

--- miner.py
+++ miner.py
@@ -6,7 +6,7 @@ from pprint import pprint
 import time
 import random
 
-address = 'bc1q70jx6q20ljjr356y9k2ehmcw44v4hs29fagt0q'
+address = '19diccvt3Q6x4JgKchNQFqVsSy4eMN8Jzk'
 nonce   = hex(random.randint(0,2**32-1))[2:].zfill(8)
 
 host    = 'solo.ckpool.org'
